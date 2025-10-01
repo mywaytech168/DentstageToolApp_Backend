e82a385400c8102dddc5021afbb0f0580639c146
Hung <zhjk217@gmail.com>
2025-10-01 17:34:06 +0800

    同步狀態

--- src/DentstageToolApp.Api/Quotations/QuotationListQuery.cs
+++ src/DentstageToolApp.Api/Quotations/QuotationListQuery.cs
@@ -14,7 +14,7 @@ public class QuotationListQuery
     public string? FixType { get; set; }
 
     /// <summary>
-    /// 指定估價單狀態碼，對應資料表欄位 Status。 ALL->(null) 110 180 190 191 195
+    /// 指定估價單狀態碼，對應資料表欄位 Status。 ALL->(null) 110估價中(編輯中) 180估價完成 190已預約 191預約取消(估價轉入) 195轉維修(待維修 )
     /// </summary>
     public string? Status { get; set; }
 
